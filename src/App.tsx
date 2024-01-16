c168afa563b75266446e4cc39d6e0acc34c95f4a
ThaSMorato <tha.s.morato@gmail.com>
2024-01-16 00:00:12 -0300

    feat: home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,86 @@
+import { useState } from 'react'
+
+import { Header } from './components/molecules/Header'
+import { NewTaskForm } from './components/organisms/NewTaskForm'
+import { Task } from './components/organisms/Task'
+import styles from './styles.module.css'
+
+interface Task {
+  id: string
+  content: string
+  isFinished: boolean
+}
+
 function App() {
-  return <h1>Hello world</h1>
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const createTask = (content: string) => {
+    const task: Task = {
+      content,
+      id: `${new Date().getTime()}`,
+      isFinished: false,
+    }
+
+    setTasks((oldTasks) => [...oldTasks, task])
+  }
+
+  const deleteTask = (id: string) => {
+    setTasks((oldTasks) => oldTasks.filter((task) => task.id !== id))
+  }
+
+  const concludeTask = (id: string) => {
+    setTasks((oldTasks) =>
+      oldTasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            isFinished: !task.isFinished,
+          }
+        }
+        return task
+      }),
+    )
+  }
+
+  const concludedTasks = tasks.filter((task) => task.isFinished).length
+
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.mainContainer}>
+          <NewTaskForm addNewTask={createTask} />
+          <div className={styles.tasksList}>
+            <div className={styles.tasksCount}>
+              <p className={styles.created}>
+                Tarefas criadas<span>{tasks.length}</span>
+              </p>
+
+              <p className={styles.concluded}>
+                Concluídas{' '}
+                <span>
+                  {concludedTasks > 0
+                    ? `${concludedTasks} de ${tasks.length}`
+                    : concludedTasks}
+                </span>
+              </p>
+            </div>
+            <div className={styles.list}>
+              {tasks.map((task) => (
+                <Task
+                  key={task.id}
+                  content={task.content}
+                  active={task.isFinished}
+                  onDeleteClick={() => deleteTask(task.id)}
+                  onFinishedClick={() => concludeTask(task.id)}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default App
